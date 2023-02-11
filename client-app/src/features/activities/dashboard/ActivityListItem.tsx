322f03ccb314f50e9e2bab558a0a5e6a60cb1816
GodSandy98 <91578767+GodSandy98@users.noreply.github.com>
2023-02-11 15:45:14 -0500

    End of Section 17

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -13,17 +13,18 @@ export default function ActivityListItem({ activity }: Props) {
     return (
         <Segment.Group>
             <Segment>
-                {activity.isCancelled && 
-                    <Label attached="top" color="red" content='Cancelled' style={{textAlign: 'center'}} />
+                {activity.isCancelled &&
+                    <Label attached="top" color="red" content='Cancelled' style={{ textAlign: 'center' }} />
                 }
                 <Item.Group>
                     <Item>
-                        <Item.Image style={{marginBottom: 5}} size='tiny' circular src='/assets/user.png' />
+                        <Item.Image style={{ marginBottom: 5 }} size='tiny' circular 
+                            src={activity.host?.image || '/assets/user.png'} />
                         <Item.Content>
                             <Item.Header as={Link} to={`/activities/${activity.id}`}>
                                 {activity.title}
                             </Item.Header>
-                            <Item.Description>Hosted by {activity.host?.displayName}</Item.Description>
+                            <Item.Description>Hosted by <Link to={`/profiles/${activity.hostUsername}`}>{activity.host?.displayName}</Link></Item.Description>
                             {activity.isHost && (
                                 <Item.Description>
                                     <Label basic color="orange">
@@ -53,7 +54,7 @@ export default function ActivityListItem({ activity }: Props) {
             </Segment>
             <Segment clearing>
                 <span>{activity.description}</span>
-                <Button 
+                <Button
                     as={Link}
                     to={`/activities/${activity.id}`}
                     color='teal'
